0afb3b7852d8adf7a83941fce3abc59afb8432e8
HYUNWOO CHEON <milcamboo@gmail.com>
2024-09-09 19:25:25 +0900

    Simple calculation

---
+++ baekjoon/n_10869.cpp
@@ -0,0 +1,15 @@
+//
+// Created by blackcow on 9/9/24.
+//
+#include <iostream>
+
+int n_10869(){
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << a * b << std::endl;
+    std::cout << a / b << std::endl;
+    std::cout << a % b << std::endl;
+    return 0;
+}
