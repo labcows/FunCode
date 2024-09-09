587f12cf9b0b120fed4ecc1a4ecb56ebca8454b4
HYUNWOO CHEON <milcamboo@gmail.com>
2024-09-09 19:27:13 +0900

    Print string

---
+++ baekjoon/n_10926.cpp
@@ -0,0 +1,12 @@
+//
+// Created by blackcow on 9/9/24.
+//
+#include <iostream>
+#include <string>
+
+int n_10926(){
+    std::string name;
+    std::cin >> name;
+    std::cout << name << "??!";
+    return 0;
+}
